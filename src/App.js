25e42c8bcdef0fdae0e2be114d3029a7b2f1f4c0
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-07 10:27:12 -0300

    react router v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </>
+
   );
 }
 
