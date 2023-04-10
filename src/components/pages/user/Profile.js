d5ed70042ec8da85fb5d52b28c5dcb8cc034d261
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-10 15:36:40 -0300

    pegar os dados do usuario para editar

--- src/components/pages/user/Profile.js
+++ src/components/pages/user/Profile.js
@@ -1,4 +1,4 @@
-
+import api from "../../../utils/api"
 import { useState, useEffect } from "react"
 
 import styles from "./Profile.module.css"
@@ -10,6 +10,20 @@ import Input from "../../form/Input"
 function Profile(){
 
     const [user, setUser] = useState({})
+    const [token] = useState(localStorage.getItem('token') || '')
+
+
+    //puxar os dados do usuario para editar
+    useEffect(()=>{
+
+        api.get('/users/checkuser', {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        }).then((res)=>{
+            setUser(res.data)
+        })
+    }, [token])
 
     function onFileChange(event){
 
@@ -24,7 +38,7 @@ function Profile(){
                 <h1>Perfil</h1>
                 <p>imagem</p>
             </div>
-            
+
             <form className={formStyles.form_container}>
                 <Input
                  text="Imagem"
