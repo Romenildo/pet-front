92813e01a4b0e094c0b4c3760db608fe69cf5aa5
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-12 13:10:53 -0300

    css dashboard pets

--- src/components/pages/Pet/MyPets.js
+++ src/components/pages/Pet/MyPets.js
@@ -27,26 +27,26 @@ function MyPets(){
     }, [token])
 
     return (
-        <section>
-            <div>
+        <section >
+            <div className={styles.petlist_header}>
                 <h1>MyPets</h1>
                 <Link to='/pet/add'>Cadastrar Pet</Link>
             </div>
             
-            <div>
+            <div className={styles.petlist_container}>
                 {pets.length > 0 && (
                     pets.map((pet)=>(
-                        <div key={pet._id}>
+                        <div key={pet._id} className={styles.petlist_row}>
                             <RoundedImage 
                                 src={`${process.env.REACT_APP_API}/imgs/pets/${pet.images[0]}` }
                                  alt={pet.name}
-                                 width="75px"
-                            />
+                                 width="px75"
+                            /> 
                             <span className="bold">{pet.name}</span>
-                            <div className={styles.action}>
+                            <div className={styles.actions}>
                                 {pet.available ? (
                                     <>
-                                    {pet.adopter && <button> Concluir adoção</button>}
+                                    {pet.adopter && <button className={styles.conclude_btn}> Concluir adoção</button>}
                                     <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
                                     <button>Excluir</button>
                                     </>
