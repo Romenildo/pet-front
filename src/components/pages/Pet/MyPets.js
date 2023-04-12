ec81756057089d60d918eb66832a4ccc03ea3cd1
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-12 10:45:02 -0300

    pegando pets do backend

--- src/components/pages/Pet/MyPets.js
+++ src/components/pages/Pet/MyPets.js
@@ -2,9 +2,27 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
+import useFlashMessage from "../../../hooks/useFlashMessage"
+import RoundedImage from "../../layout/RoundedImage"
+import api from "../../../utils/api"
+
 function MyPets(){
 
     const [pets, setPets] = useState([])
+    const [token] = useState(localStorage.getItem('token') || '')
+    const { setFlashMessage } = useFlashMessage()
+    
+    //puxar os pets do banco
+    useEffect(()=>{
+
+        api.get('/pets/mypets', {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`
+            }
+        }).then((res)=>{
+            setPets(res.data.pets)
+        })
+    }, [token])
 
     return (
         <section>
@@ -14,8 +32,8 @@ function MyPets(){
             </div>
             
             <div>
-                {pets.length > 0 && (<p>Meus Pets cadastrados</p>)}
-                {pets.length === 0 && (<p>Nenhum Pet cadastrado</p>)}
+                {pets.length > 0 && <p>Meus Pets cadastrados</p>}
+                {pets.length === 0 && <p>Nenhum Pet cadastrado</p>}
             </div>
         </section>
     )
